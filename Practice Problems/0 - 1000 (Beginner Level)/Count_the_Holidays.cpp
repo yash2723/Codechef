fe5ec412f5c751b500f7d4ab9e945ef9c1d03107
yash <yash.upadhyay.2723@gmail.com>
2022-03-05 17:37:23 +0530

    Second day (04-03-22) Practice Problems

---
+++ Practice Problems/0 - 1000 (Beginner Level)/Count_the_Holidays.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PI  3.141592653589793
+
+#define FAST ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define ll long long
+#define vi vector <int>
+#define vll vector < ll >
+#define pb push_back
+#define endl '\n'
+#define max3(a,b,c) max(max(a,b),c)
+#define min3(a,b,c) min(min(a,b),c)
+#define answer(ans) cout << ans << '\n' 
+#define ansyes cout << "YES\n" 
+#define ansno cout << "NO\n" 
+#define pii pair <int,int>
+
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll arr[n] , count = 8;
+    for(int i = 0 ; i < n ; i++)
+    {
+        cin >> arr[i];
+        if((arr[i] % 7 != 0) && ((arr[i]+1) % 7 != 0))
+            count++;
+    }
+    answer(count);
+}
+
+
+int main()
+{
+    FAST;
+
+    ll t = 1;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    cerr << "time taken : " << (float)clock()/CLOCKS_PER_SEC << " secs" << endl;
+
+    return 0;
+}
---
+++ Practice Problems/0 - 1000 (Beginner Level)/Which_Fuel_is_Cheaper.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PI  3.141592653589793
+
+#define FAST ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define ll long long
+#define vi vector <int>
+#define vll vector < ll >
+#define pb push_back
+#define endl '\n'
+#define max3(a,b,c) max(max(a,b),c)
+#define min3(a,b,c) min(min(a,b),c)
+#define answer(ans) cout << ans << '\n' 
+#define ansyes cout << "YES\n" 
+#define ansno cout << "NO\n" 
+#define pii pair <int,int>
+
+
+void solve()
+{
+    ll x , y , a , b , k;
+    cin >> x >> y >> a >> b >> k;
+    ll petrol = x + (a*k);
+    ll diesel = y + (b*k);
+    if(petrol < diesel)
+        answer("PETROL");
+    else if(petrol > diesel)
+        answer("DIESEL");
+    else
+        answer("SAME PRICE");
+}
+
+
+int main()
+{
+    FAST;
+
+    ll t = 1;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    cerr << "time taken : " << (float)clock()/CLOCKS_PER_SEC << " secs" << endl;
+
+    return 0;
+}
