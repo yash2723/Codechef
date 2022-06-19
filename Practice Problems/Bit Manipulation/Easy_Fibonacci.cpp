948cfabcd7f49150dac571effd428484b790ac4a
yash <yash.upadhyay.2723@gmail.com>
2022-06-19 23:36:57 +0530

    Updated files till Date : 19th June 2022

--- Practice Problems/Bit Manipulation/Easy_Fibonacci.cpp
+++ Practice Problems/Bit Manipulation/Easy_Fibonacci.cpp
@@ -16,19 +16,32 @@ using namespace std;
 #define ansno cout << "NO\n" 
 #define pii pair <int,int>
 
+ll arr[60];
+void init()
+{
+    arr[0] = 0;
+    arr[1] = 1;
+    for(int i = 2 ; i < 60 ; i++)
+    {
+        ll sum = arr[i-1] + arr[i-2];
+        arr[i] = sum;
+    }
+}
 
 void solve()
 {
     ll n;
     cin >> n;
-    answer((ll)log2(n));
+    ll bits = ((ll)log2(n));
+    ll index = pow(2,bits)-1;
+    answer((arr[index%60])%10);
 }
 
 
 int main()
 {
     FAST;
-
+    init();
     ll t = 1;
     cin >> t;
     while(t--)
