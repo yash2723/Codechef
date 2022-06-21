4b85ca99aa4d5d8181b375155e6a3ff8ef09acbb
yash <yash.upadhyay.2723@gmail.com>
2022-06-21 22:21:43 +0530

    Updated files till Date : 21th June 2022

---
+++ June Lunchtime 2022 Division 2 (Rated)/Equal_by_XORing.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PI  3.141592653589793
+
+#define FAST ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define ll long long int
+#define vi vector <int>
+#define vll vector < ll >
+#define pb push_back
+#define endl '\n'
+#define max3(a,b,c) max(max(a,b),c)
+#define min3(a,b,c) min(min(a,b),c)
+#define answer(ans) cout << ans << '\n' 
+#define ansyes cout << "YES\n" 
+#define ansno cout << "NO\n" 
+#define pii pair <int,int>
+
+
+void solve()
+{
+    ll a , b , n;
+    cin >> a >> b >> n;
+    if(a == b)
+    {
+        answer(0);
+        return;
+    }
+    ll ans = a ^ b;
+    if(ans >= 1 && ans < n)
+        answer(1);
+    else if((n & (n-1)) && (ans <= (pow(2,(int)log2(n)+1)-1)))
+        answer(2);
+    else
+        answer(-1);
+}
+
+
+int main()
+{
+    FAST;
+
+    ll t = 1;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    cerr << "time taken : " << (float)clock()/CLOCKS_PER_SEC << " secs" << endl;
+
+    return 0;
+}
