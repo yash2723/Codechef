8877e2b00b3b5d7f1575e3dc56c2c350603eec02
Yash Upadhyay <yash.upadhyay.2723@gmail.com>
2023-02-28 00:58:53 +0530

    Date : 28 - 2 - 23

---
+++ Starters 73 Division 2 (Rated)/Append_for_OR.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PI  3.141592653589793
+
+#define FAST ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define ll long long int
+#define vi vector <int>
+#define vll vector < ll >
+#define pb push_back
+#define endl '\n'
+#define max3(a,b,c) max(max(a,b),c)
+#define min3(a,b,c) min(min(a,b),c)
+#define answer(ans) cout << ans << '\n' 
+#define ansyes cout << "YES\n" 
+#define ansno cout << "NO\n" 
+#define pii pair <int,int>
+
+
+void solve()
+{
+    ll n , y;
+    cin >> n >> y;
+
+    vll a(n);
+    ll res = 0;
+    for(int i = 0 ; i < n ; i++) {
+        cin >> a[i];
+        res |= a[i];
+    }
+
+    ll ans = res ^ y;
+
+    if((ans | res) != y)
+        answer(-1);
+    else
+        answer(ans);
+}
+
+
+int main()
+{
+    FAST;
+
+    ll t = 1;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    cerr << "time taken : " << (float)clock()/CLOCKS_PER_SEC << " secs" << endl;
+
+    return 0;
+}
